bfbe0f8833b650593d34fc1bec13732187aa5e1d
William T <wftjen@gmail.com>
2023-11-13 09:39:00 -0500

    basic navbar stub

---
+++ src/app/navbar.js
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+
+const navigation = [
+  { name: 'About', href: '#' },
+  { name: 'Projects', href: '#' },
+  { name: 'Resume', href: '#' },
+  { name: 'Contact', href: '#' },
+]
+
+export default function Navbar() {
+    return (
+        <header className="mb-10 z-50">
+            <Link href="/">
+                <div className="flex items-center mb-4">
+                    <div className="mr-4">
+                        <img
+                            className="h-8 w-auto"
+                            src="https://tailwindui.com/img/logos/mark.svg?color=rose&shade=400"
+                            alt=""
+                        />
+                    </div>
+                    <span className="text-lg font-bold">William Tietjen</span>
+                </div>
+            </Link>
+            <nav className="flex items-center">
+                <div className="flex gap-x-8">
+                    {navigation.map((item) => (
+                        <Link key={item.name} href={item.href} className="text-md leading-6 hoverable">
+                            {item.name}
+                        </Link>
+                    ))}
+                </div>
+            </nav>
+        </header>
+    )
+}
