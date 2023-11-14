4944e38050d63a2a5fb4491c38a876f1759da351
William T <wftjen@gmail.com>
2023-11-14 10:24:11 -0500

    navbar responsiveness

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -11,8 +11,8 @@ export default function Navbar() {
     return (
         <header className="mb-10 z-50">
             <Link href="/">
-                <div className="flex items-center mb-4">
-                    <div className="mr-4">
+                <div className="flex flex-col lg:flex-row items-center mb-2 lg:mb-4">
+                    <div className="lg:mr-4 mb-2 lg:mb-0">
                         <img
                             className="h-8 w-auto"
                             src="https://tailwindui.com/img/logos/mark.svg?color=rose&shade=400"
@@ -22,10 +22,10 @@ export default function Navbar() {
                     <span className="text-lg font-bold">William Tietjen</span>
                 </div>
             </Link>
-            <nav className="flex items-center">
-                <div className="flex gap-x-8">
+            <nav className="flex justify-center lg:justify-start">
+                <div className="flex gap-x-3 lg:gap-x-8">
                     {navigation.map((item) => (
-                        <Link key={item.name} href={item.href} className="text-md leading-6 hoverable">
+                        <Link key={item.name} href={item.href} className="text-sm lg:text-base leading-6 hoverable">
                             {item.name}
                         </Link>
                     ))}
