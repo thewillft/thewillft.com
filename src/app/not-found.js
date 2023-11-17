afdec92e8528ccb0953e8bf7e912d900bbf5c8cc
William T <wftjen@gmail.com>
2023-11-16 19:48:45 -0500

    added custom 404 error page

---
+++ src/app/not-found.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+ 
+export default function NotFound() {
+  return (
+    <main className="flex flex-1">
+        <div className="m-auto flex flex-col justify-center items-center">
+            <h2 className="text-xl text-rose-400 font-semibold">Not Found</h2>
+            <p className="text-neutral-400">Could not find requested resource</p>
+            <Link href="/" className="hoverable">Go Home →</Link>
+        </div>
+    </main>
+  )
+}
